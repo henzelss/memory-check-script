451004f4d680a3e2089623405ce79052c8426763
henzelprosperna <henzel@prosperna.com>
2025-01-27 00:58:37 +0800

    Added top 5 processes

--- memorycheck.sh
+++ memorycheck.sh
@@ -5,3 +5,6 @@ echo "Memory Check - $(date)"
 echo "-------------------------"
 free -h
 echo "-------------------------"
+echo "Top 5 Memory-Consuming Processes:"
+ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head -n 6
+echo "-------------------------"
